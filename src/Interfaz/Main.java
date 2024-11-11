9a5009b7217059d2860a773557293a885a990b20
pipetorrendell <pipetorrendell@localhost>
2024-11-11 10:16:52 -0300

    Ventanas Hechas

--- src/Interfaz/Main.java
+++ src/Interfaz/Main.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Interfaz;
 
 /**
@@ -9,7 +5,9 @@ package Interfaz;
  * @author pipetorrendell
  */
 public class Main {
+
     public static void main(String[] args) {
-        
+        VentanaMenu ventanaMenu = new VentanaMenu();
+        ventanaMenu.setVisible(true);
     }
 }
